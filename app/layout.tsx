93a08692048c536d46f7941ae67ca971ee5c7fdd
Kunal Garg <kunalgarg054@gmail.com>
2025-09-27 15:48:18 +0530

    vercel analytics fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,7 +22,6 @@ export const metadata: Metadata = {
     title,
     description,
     siteName: title,
-    url: 'https://storage.googleapis.com/person-website-kunal-garg/person-graph-og-img.png',
   },
   creator: 'Kunal Garg',
 };
